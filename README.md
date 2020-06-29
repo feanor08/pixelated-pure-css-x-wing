22b67574f79723e5c5302045a9ff824d979d5407
Adarsh ML <adarsh.ml@atherenergy.com>
2020-06-29 10:45:25 +0530

    Add version 1.0.3 Of Package

---
+++ README.md
@@ -0,0 +1,3 @@
+# X Wing : Pixel Art with Pure CSS
+A Pure Css X Wing Fighter Component. 
+This was made with only CSS. No images were used. 
---
+++ src/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+
+import './App.css';
+
+
+function PixelPureCssXWing() {
+  return (
+    <div className="x-wing-pixel">
+      <div className="rotate">
+      <div className="x-wing">
+        <div className="wing">
+          <div className="wing-one " />
+          <div className="wing-two " />
+          <div className="wing-three " />
+          <div className="wing-four " />
+        </div>
+        <div className="head test">
+          <div className="head-zero " />
+          <div className="head-one " />
+          <div className="head-two " />
+          <div className="head-three " />
+          <div className="head-four " />
+        </div>
+        <div className="tail">
+          <div className="tail-one " />
+          <div className="tail-two " />
+          <div className="tail-three " />
+          <div className="tail-four " />
+          <div className="tail-five " />
+
+        </div>
+        <div className="side-thruster left-thruster">
+          <div className="st-one " />
+          <div className="st-separator" />
+          <div className="st-two " />
+          <div className="st-three " />
+        </div>
+        <div className="side-thruster right-thruster">
+          <div className="st-one " />
+          <div className="st-separator" />
+          <div className="st-two " />
+          <div className="st-three " />
+        </div>
+        <div className="cannon left-cannon ">
+          <div className="can-one can-top-left"/>
+          <div className="can-blank " />
+          <div className="can-blank  " />
+          <div className="can-two"/>
+          <div className="can-blank  " />
+          <div className="can-two"/>
+          <div className="can-blank " />
+          <div className="can-two"/>
+        </div>
+        <div className="cannon right-cannon ">
+          <div className="can-one can-top-right"/>
+          <div className="can-blank " />
+          <div className="can-blank  " />
+          <div className="can-two"/>
+          <div className="can-blank  " />
+          <div className="can-two"/>
+          <div className="can-blank " />
+          <div className="can-two"/>
+        </div>
+        <div className="wing-decor wd-left ">
+          <div className="wd-one"/>
+          <div className="wd-two" />
+          <div className="wd-three" />
+          <div className="wd-four"/>
+          <div className="wd-five"/>
+        </div>
+        <div className="wing-decor wd-right flip-horizontal ">
+          <div className="wd-one"/>
+          <div className="wd-two" />
+          <div className="wd-three" />
+          <div className="wd-four"/>
+          <div className="wd-five"/>
+        </div>
+        <div className="side-stripes">
+          <div className="stripes  left-stripes">
+            <div className="stripes-big" />
+            <div className="stripes-medium" />
+            <div className="stripes-small" />
+          </div>
+          <div className="stripes right-stripes">
+            <div className="stripes-big" />
+            <div className="stripes-medium" />
+            <div className="stripes-small" />
+          </div>
+        </div>
+        <div className="body-art  ">
+          <div className="ba-blank-one"/> 
+          <div className="ba-one"/>
+          <div className="ba-blank-two"/> 
+          <div className="ba-two" />
+          <div className="ba-blank-three"/> 
+          <div className="ba-three" />
+          <div className="ba-blank-four"/> 
+          <div className="ba-four"/>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default PixelPureCssXWing;
